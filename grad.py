3b1fe6492b38a0699e4d10e2a0c545bdff31111b
Rob <robahall2@gmail.com>
2019-04-14 15:33:12 -0400

    Adding gradient descent function

--- grad.py
+++ grad.py
@@ -11,26 +11,34 @@ def generate_dataset(n):
     """Generates uniform random dataset for gradient descent algos."""
 
     x = np.ones([n, 2])
-    x[:, 0] = np.random.uniform(-1,1,n)
+    x[:, 1] = np.random.uniform(-1,1,n)
 
-    a = np.array([5., 2])
-    b = np.random.rand(1, n)
-    y = np.dot(x, a) + b
+    theta = np.array([2, 5.])
+    jitter = np.random.rand(1, n)
+    y = np.dot(x, theta) + jitter
 
     return x, y
 
-def sgd(x, y):
-    theta = np.arrary([-1., 1.])
-    hypothesis = np.dot(x, theta)
-    loss = hypothesis - y
-    cost = np.sum(loss**2)/(2*m)
+def gd(x, y, theta, alpha):
+    m = X.shape[0]
+    print(m)
+    grad = np.dot(X, theta) - y
+    print(X)
+    print(theta)
+    print(grad)
+    return theta - alpha * (1/m) * np.sum(grad)
 
 
 
 
 
 if __name__ == "__main__":
-    x, y = generate_dataset(100)
+    X, y = generate_dataset(100)
 
-    plt.scatter(x[:, 0], y)
+    plt.scatter(X[:, 1], y)
     plt.savefig('figure.png')
+
+    theta = np.array([1., 2])
+    learning_rate = 0.01
+    update = gd(X, y, theta, learning_rate)
+    print(update)
