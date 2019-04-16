5bb8ebe41750af8fbe1e052b44525c5060d86a5f
Rob Hall <rhall@sonalysts.com>
2019-04-16 14:20:52 -0400

    Updated to create class structure for data generation. Added generic gradient descent.

--- data_generation.py
+++ data_generation.py
@@ -1,6 +1,9 @@
 import numpy as np
 from sklearn.datasets import make_regression
 
+import matplotlib.pyplot as plt
+plt.interactive(False)
+
 class Data(object):
     """Generate regression random numpy data set
     """
@@ -28,9 +31,35 @@ class Data(object):
     def inspectY(self):
         return self.Y
 
+def gd(dataset, theta, alpha):
+    """The standard gradient descent algo."""
+    m = dataset.X.shape[0]
+    error = dataset.X @ theta - dataset.Y
+    grad = dataset.X.transpose() @ error ## multiplying each of the X's specific to features by the error it was off by
+    theta = theta - alpha * (1/m) * grad
+    MSE = np.average(error ** 2)
+    return theta, MSE
+
+
 
 if __name__ == "__main__":
     data = Data(samples=10, features=3, informative=3, targets=1, jitter=0.1)
-    print(data.inspectX())
-    print(data.inspectY())
-    print(data.inspectCoef())
+
+    theta = np.array([1., 1, 1])
+    learning_rate = 0.01
+    update, MSE = gd(data, theta, learning_rate)
+
+    for i in range(1000):
+        update, MSE = gd(data, update, learning_rate)
+        theta = np.vstack((theta, update))
+        print("Iteration: {}  MSE: {}".format(i, MSE))
+
+    print(theta[999])
+
+    print(data.coef)
+    plt.scatter(theta[:, 0], theta[:, 1], theta[:, 2])
+    plt.ylim(0,10)
+    plt.xlim(0, 10)
+    plt.savefig('gd.png')
+
+
