fbe58c5fd853bb7d2520a1bf3dd0e6e5b620533b
Rob Hall <rhall@sonalysts.com>
2019-04-18 12:05:36 -0400

    Fixed Algo

--- src/grad_desc_algos.py
+++ src/grad_desc_algos.py
@@ -5,8 +5,9 @@ def gd(X, y, theta, learning_rate):
     """The standard gradient descent algo."""
     m = X.shape[0]
     error = np.dot(X, theta) - y
-    grad = np.transpose((np.dot(X, theta) - y)) * X
-    sum = np.sum(grad, axis = 0)
-    update = theta - learning_rate * (1/m) * sum
+    grad = np.dot(error, X)
+    update = theta - learning_rate * (1/m) * grad
     MSE = np.average(error ** 2)
-    return update, error, MSE
+    return update, MSE
+
+
