c575bd6314605a176834d33571311cbc5f2537d0
robahall <robahall2@gmail.com>
2019-05-06 11:26:49 -0400

    Tested latest update on grad_desc_algos.py

--- src/test.py
+++ src/test.py
@@ -15,14 +15,17 @@ if __name__ == "__main__":
 
     theta = np.array([120, 40.])
     lr = 0.01
-    epochs = 1
+    epochs = 100
     momentum = 0
 
-    thetas, MSE, count = gda.momentum_gd(features, target, theta, lr, epochs, momentum, batch_size = 1)
 
-    print(thetas[-1,:])
-    print(coef)
-    print(MSE[-1,:])
+
+    thetas, MSE, count = gda.momentum_gd(features, target, theta, lr, epochs, momentum, batch_size=1)
+
+    print("Determined weights: {}".format(thetas[-1,:]))
+    print("Acutal Coefficients: {}".format(coef))
+    print("End Mean Squared Error: {}".format(MSE[-3:,1]))
+    print("Number of epochs: {}".format(count))
 
 
 ## See how my code stacks up to this: https://gluon.mxnet.io/chapter06_optimization/momentum-scratch.html
